7863cdfa3b646a32254474af422e98544985af02
sungchan12 <chanyouunng1@gmail.com>
2026-03-29 22:01:38 +0900

    1. 각 횟수별 실행 결과 출력  2. 최종 우승자 출력  3. 공동 우승자 , 구분 출력

--- src/main/java/racingcar/entity/Cars.java
+++ src/main/java/racingcar/entity/Cars.java
@@ -14,4 +14,36 @@ public class Cars {
             car.setMoveCnt();
         }
     }
+
+    public void printMoveResult() {
+        for (Car car : this.cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getMoveCnt()));
+        }
+        System.out.println();
+    }
+
+    private int findMaxMoveCnt() {
+        int max = 0;
+        for (Car car : this.cars) {
+            if (car.getMoveCnt() > max) {
+                max = car.getMoveCnt();
+            }
+        }
+        return max;
+    }
+
+    private ArrayList<String> findWinners(int max) {
+        ArrayList<String> winners = new ArrayList<>();
+        for (Car car : this.cars) {
+            if (car.getMoveCnt() == max) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
+    public void printWinner() {
+        ArrayList<String> winners = findWinners(findMaxMoveCnt());
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
