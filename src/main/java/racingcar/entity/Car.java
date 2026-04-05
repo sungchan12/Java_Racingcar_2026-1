1de88aaa16a6e120759e75b5a9554b5f0b74de11
sungchan12 <chanyouunng1@gmail.com>
2026-04-05 21:14:36 +0900

    refactor : Car 자신의 이름을 검증하게 수정

--- src/main/java/racingcar/entity/Car.java
+++ src/main/java/racingcar/entity/Car.java
@@ -1,7 +1,6 @@
 package racingcar.entity;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import racingcar.util.Utils;
 
 public class Car {
     private final String name;
@@ -9,7 +8,7 @@ public class Car {
     private final int MOVE_THRESHOLD = 4;
 
     public Car(String name) {
-        Utils.isValidName(this.name = name);
+        validateName(this.name = name);
         this.moveCnt = 0;
     }
 
@@ -24,6 +23,15 @@ public class Car {
     public void setMoveCnt() {
         if (Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD) {
             this.moveCnt++;
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank() || name.contains(" ")) {
+            throw new IllegalArgumentException("이름 공백 또는 빈값은 안됩니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("5글자 초과했습니다.");
         }
     }
 }
