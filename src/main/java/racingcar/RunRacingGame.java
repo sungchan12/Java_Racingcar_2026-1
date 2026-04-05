24cb63df6dfc4b4d192dc2a55f6e6d1d4f8f244a
sungchan12 <chanyouunng1@gmail.com>
2026-04-05 20:47:55 +0900

    refactor: [input 값과 output결과 보여주는 책임을 분리]

--- src/main/java/racingcar/RunRacingGame.java
+++ src/main/java/racingcar/RunRacingGame.java
@@ -1,33 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-import racingcar.entity.Car;
-import racingcar.entity.Cars;
-
-import java.util.ArrayList;
-
-import static racingcar.util.Utils.isValidNumber;
-import static racingcar.util.Utils.splitNames;
 
 public class RunRacingGame {
     public void run() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = splitNames(Console.readLine());
-        System.out.println("시도할 회수는 몇회인가요?");
-        int number = isValidNumber(Console.readLine());
-
-        ArrayList<Car> carsList = new ArrayList<>();
-        for (String carName : carNames) {
-            Car car = new Car(carName);
-            carsList.add(car);
-        }
-
-        System.out.println("\n실행 결과");
-        Cars cars = new Cars(carsList);
-        for (int i = 0; i < number; i++) {
-            cars.moveAllCars();
-            cars.printMoveResult();
-        }
-        cars.printWinner();
+        //TODO : Input과 Output 출력하는 책임을 분리 진행
+        private racingCarInput input = new racingCarInput();
+        private racingCarOutput output = new racingCarOutput();
     }
 }
